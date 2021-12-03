c0901206a58eb0361389d53428e24b2ce078ad87
Nicola <nicola.messina93@gmail.com>
2021-12-03 16:20:32 +0100

    Sparse linear assignment search

--- inference.py
+++ inference.py
@@ -11,30 +11,34 @@ import csv
 import pandas as pd
 import os
 import numpy as np
-from scipy.optimize import linear_sum_assignment
+from scipy import sparse as sp
+import json
 
 import evaluation
 import utils
 from dataset import WikipediaDataset, collate_fn_without_nones
 from model import MatchingModel
 
-SCORES_CACHE = 'cached_scores.npy'
-
+SCORES_CACHE = 'cached_scores.npz'  # sparse
+IDXS_CACHE = 'cached_indexes.npy'  # dense
 
 def knn_search_from_cached_scores(npts, topk=5):
-    fp = np.memmap(SCORES_CACHE, dtype='float32', mode='r', shape=(npts, npts))
+    indexes = np.load(IDXS_CACHE)
+    indexes = indexes[:, :topk]
+    return indexes.tolist()
 
-    ordered = np.argsort(fp, axis=1)[:, ::-1]
-    ordered = ordered[:, :topk]
-    results = ordered.tolist()
-    return results
+    # inds, _ = utils.sparse_topk(scores, k=topk, reverse=True, return_mat=False)
+    # df = pd.DataFrame(zip(*inds))
+    # lol = df.groupby(0)[1].apply(list).tolist()
+    # return lol
 
 
-def exhaustive_knn_search(query_feats, caption_feats, topk=5, batch_size=5, gpu=False, cache_scores=False):
+def exhaustive_knn_search(query_feats, caption_feats, topk=5, batch_size=5, gpu=False, cache_scores=False, top_scores=1000):
     npts = query_feats.shape[0]
     if cache_scores and not os.path.exists(SCORES_CACHE):
         logging.info('Caching scores into {}'.format(SCORES_CACHE))
-        fp = np.memmap(SCORES_CACHE, dtype='float32', mode='w+', shape=(npts, npts))
+        cached_scores = sp.csr_matrix((npts, npts), dtype='float32')
+        cached_indexes = np.empty((npts, top_scores))
 
     num_batches = (npts // batch_size) + 1 if npts % batch_size != 0 else (npts // batch_size)
     pbar = tqdm.trange(num_batches)
@@ -50,25 +54,41 @@ def exhaustive_knn_search(query_feats, caption_feats, topk=5, batch_size=5, gpu=
         # Compute scores
         d = torch.mm(query, caption_feats.T)  # query_batch_size x npts
 
-        ranks = torch.argsort(d, dim=1, descending=True)
+        scores, ranks = torch.sort(d, dim=1, descending=True)
         ordered = ranks[:, :topk]    # get top k
         ordered = ordered.tolist()
         results.extend(ordered)
 
         if cache_scores:
-            fp[index * batch_size: (index + 1) * batch_size, :] = d.cpu().numpy()
+            for i in range(d.shape[0]):
+                internal_idx = index * batch_size + i
+                r = ranks[i, :top_scores].cpu().numpy()
+                s = scores[i, :top_scores]
+
+                cached_scores[internal_idx, r] = s.cpu().numpy() # save only the first 'top_scores' indexes
+                cached_indexes[internal_idx, :] = r
 
     if cache_scores:
-        fp.flush()
+        sp.save_npz(SCORES_CACHE, cached_scores)
+        np.save(IDXS_CACHE, cached_indexes)
     return results
 
 
-def linear_assignment_search(npts, topk=5):
-    scores = np.memmap(SCORES_CACHE, dtype='float32', mode='c', shape=(npts, npts))
+def linear_assignment_search(topk=5, top_scores=1000):
+    scores = sp.load_npz(SCORES_CACHE)
+    indexes = np.load(IDXS_CACHE).astype(int)
+    npts = indexes.shape[0]
+    if top_scores < indexes.shape[1]:
+        dense_scores = scores.todense()
+        np.put_along_axis(dense_scores, indexes[:, top_scores:], 0, axis=1)
+        # dense_scores[indexes[:, top_scores:]] = 0
+        #dense_scores = np.take(dense_scores, indexes[:, top_scores:], axis=1)
+        scores = sp.csr_matrix(dense_scores)
+
     result = np.empty((npts, topk))
     # scores = np.load(SCORES_CACHE)
     for i in tqdm.trange(topk):
-        row_ind, col_ind = linear_sum_assignment(scores, maximize=True)
+        row_ind, col_ind = sp.csgraph.min_weight_full_bipartite_matching(scores, maximize=True)
         scores[row_ind, col_ind] = 0
         result[row_ind, i] = col_ind
     result = result.tolist()
@@ -107,6 +127,20 @@ def compute_recall(items, topk):
     return {'r1':r1, 'r5': r5, 'r10':r10}
 
 
+def compute_la_stats(opt):
+    assert os.path.exists(SCORES_CACHE)
+    logging.info('Computing stats on linear assignment for different values of top_scores')
+    rows = []
+    top_k_scores = [300, 350] # list(range(300, 1000, 50))
+    for t in tqdm.tqdm(top_k_scores):
+        result_indexes = linear_assignment_search(opt.k, top_scores=t)
+        metrics = compute_recall(result_indexes, topk=opt.k)
+        row = {'top_scores': t, 'r1': metrics['r1'], 'r5': metrics['r5']}
+        rows.append(row)
+    with open(opt.compute_la_stats, 'w') as f:
+        json.dump(rows, f)
+
+
 def main(opt):
     checkpoint = torch.load(opt.checkpoint, map_location='cpu')
     config = checkpoint['cfg']
@@ -121,7 +155,7 @@ def main(opt):
     if opt.linear_assignment:
         assert opt.enable_cached_scores and os.path.exists(SCORES_CACHE)
         logging.info('Using cached scores. Linear assignment search')
-        result_indexes = linear_assignment_search(n_samples, opt.k)
+        result_indexes = linear_assignment_search(opt.k, top_scores=700)
     else:
         if opt.enable_cached_scores and os.path.exists(SCORES_CACHE):
             logging.info('Using cached scores. Standard search')
@@ -213,6 +247,7 @@ if __name__ == '__main__':
     parser.add_argument('--img_cache', type=str, default=None, help='Path to images')
     parser.add_argument('--set', type=str, default='test', choices=['test', 'val'], help='Which set to use for inference')
     parser.add_argument('--linear_assignment', action='store_true', help='Enable linear assignment')
+    parser.add_argument('--compute_la_stats', type=str, default=None, help='If a json file is specified, this is where where la stats are saved')
     parser.add_argument('--disable_cached_scores', action='store_true', help='Disables loading of the cached scores')
     parser.add_argument('--train_subfolder', type=str, default='full', help='Which train feather files are used (for constructing the validation set')
     parser.add_argument('--bs', type=int, default=64)
@@ -221,4 +256,7 @@ if __name__ == '__main__':
     opt.enable_cached_scores = not opt.disable_cached_scores
     print(opt)
 
-    main(opt)
+    if opt.compute_la_stats is not None:
+        compute_la_stats(opt)
+    else:
+        main(opt)
