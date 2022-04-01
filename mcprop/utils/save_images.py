0c34013f61b5edc3779faea73921e84e7be4eff3
Davide Coccomini <davidecoccomini@gmail.com>
2022-04-01 17:10:27 +0200

    Update save_images.py
    Move variables from static to command line

--- mcprop/utils/save_images.py
+++ mcprop/utils/save_images.py
@@ -11,10 +11,11 @@ from tqdm import tqdm
 from multiprocessing.pool import Pool
 from functools import partial
 
-BASE_DIR = "../data/image_data_train/image_pixels"
-OUTPUT_DIR = "../data/image_data_train/images"
+import argparse
+
+
 def save_images(csv_file):
-    df = pd.read_csv(os.path.join(BASE_DIR, csv_file), compression='gzip', sep="\t")
+    df = pd.read_csv(os.path.join(opt.images_path, csv_file), compression='gzip', sep="\t")
     rows = len(df.index)
     for index, row in enumerate(df.iterrows()):
         url = row[1][0]
@@ -22,15 +23,26 @@ def save_images(csv_file):
         image_name = str(uuid.uuid5(uuid.NAMESPACE_URL, url))
         
         decoded = base64.b64decode(base64_string)
-        output_file = open(os.path.join(OUTPUT_DIR, image_name + ".jpg"), 'w', encoding="ISO-8859-1")
+        output_file = open(os.path.join(opt.output_path, image_name + ".jpg"), 'w', encoding="ISO-8859-1")
         output_file.write(decoded.decode("ISO-8859-1"))
         output_file.close()
 
         if index % 10000 == 0 and index > 0:
             print("Saved", index, "/", rows ,"images for csv", csv_file)
-     
-paths = os.listdir(BASE_DIR)
-with Pool(processes=cpu_count()-2) as p:
-    with tqdm(total=len(paths)) as pbar:
-        for v in p.imap_unordered(partial(save_images), paths):
-            pbar.update()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()  
+    parser.add_argument('--images_path', default="../../data/image_data_train/image_pixels", type=str,
+                        help='Path to the images_pixels folder')
+    parser.add_argument('--output_path', default="../../data/image_data_train/images", type=str,
+                        help='Path to the output folder')
+    opt = parser.parse_args()
+    print(opt)
+
+    paths = os.listdir(opt.images_path)
+    with Pool(processes=cpu_count()) as p:
+        with tqdm(total=len(paths)) as pbar:
+            for v in p.imap_unordered(partial(save_images), paths):
+                pbar.update()
